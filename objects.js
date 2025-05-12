8ebffdee9ae5b83958cbdc04bf86c52331128a52
HarsH <jharsh0002@gmail.com>
2025-05-12 19:37:17 +0530

    learned objects in TS

---
+++ objects.js
@@ -0,0 +1,11 @@
+var userinfo = {
+    name: 'Jha',
+    age: 25,
+    isActive: true,
+};
+function getUserInfo(
+// object as parameter
+) {
+    // actual code
+    return {};
+}
---
+++ objects.ts
@@ -0,0 +1,14 @@
+const userinfo = {
+    name: 'Jha',
+    age: 25,
+    isActive: true,
+}
+
+function getUserInfo(
+    // object as parameter
+):{
+    // return types as they are object 
+} {
+    // actual code
+    return {}
+}
