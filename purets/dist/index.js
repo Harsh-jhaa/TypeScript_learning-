757d6b6cd61836605aac423666a92204f0d30373
HarsH <jharsh0002@gmail.com>
2025-05-14 02:44:21 +0530

    learned classes in TS

--- purets/dist/index.js
+++ purets/dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log('Hello, PureTS!');
-console.log('This is a TypeScript file.');
+// console.log('Hello, PureTS!');
+// console.log('This is a TypeScript file.');
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+}
+const jha = new User('jha@.com', 'Jha');
--- purets/src/index.ts
+++ purets/src/index.ts
@@ -1,2 +1,15 @@
-console.log('Hello, PureTS!');
-console.log('This is a TypeScript file.');
+// console.log('Hello, PureTS!');
+// console.log('This is a TypeScript file.');
+
+class User {
+  // predefined properties
+  email: string;
+  name: string;
+
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const jha = new User('jha@.com', 'Jha');
